75236ab7e764d049abc99344de958ac2205e989a
bartek <smalecbartlomiej@gmail.com>
2023-07-31 16:06:49 +0200

    Test jenkins deploy on GKE

--- src/main/java/com/barteksmalec/springoktademo/controllers/HomeController.java
+++ src/main/java/com/barteksmalec/springoktademo/controllers/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
     @GetMapping("/")
     public String getHome(){
-        return "Hello World";
+        return "Hello World - Test jenkins deploy to GKE";
     }
 }
